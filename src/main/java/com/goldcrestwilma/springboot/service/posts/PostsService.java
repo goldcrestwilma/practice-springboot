da362444a62a50589063fd471029ff4fe8ca2c3d
goldcrestwilma <aga601@naver.com>
2021-01-12 23:47:31 +0900

    Spring Data JPA - save

---
+++ src/main/java/com/goldcrestwilma/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.goldcrestwilma.springboot.service.posts;
+
+import com.goldcrestwilma.springboot.domain.posts.PostsRepository;
+import com.goldcrestwilma.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
