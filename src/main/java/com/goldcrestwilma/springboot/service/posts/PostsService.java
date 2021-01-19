5b6045a3c5637c04b7588b19bc21a0b6a10712b6
goldcrestwilma <aga601@naver.com>
2021-01-20 00:14:31 +0900

    Add update, findById service

--- src/main/java/com/goldcrestwilma/springboot/service/posts/PostsService.java
+++ src/main/java/com/goldcrestwilma/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.goldcrestwilma.springboot.service.posts;
 
+import com.goldcrestwilma.springboot.domain.posts.Posts;
 import com.goldcrestwilma.springboot.domain.posts.PostsRepository;
+import com.goldcrestwilma.springboot.web.dto.PostsResponseDto;
 import com.goldcrestwilma.springboot.web.dto.PostsSaveRequestDto;
+import com.goldcrestwilma.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("The post does not exist. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("The post does not exist. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
