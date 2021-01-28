2087039e5dc11ff17cb2d1309d8c1576200e0866
goldcrestwilma <aga601@naver.com>
2021-01-28 21:07:15 +0900

    Improve base on annotation - SessionUser

--- src/main/java/com/goldcrestwilma/springboot/config/auth/README.md
+++ src/main/java/com/goldcrestwilma/springboot/config/auth/README.md
@@ -21,4 +21,13 @@
 ### userInfoEndpoint
  - OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
 
+## LoginUserArgumentResolver.class
+### @Target(ElementType.PARAMETER)
+ - 이 어노테이션이 생성될 수 있는 위치를 지정
+ - `PARAMETER` 로 지정했으니 메소드의 파라미터로 선언된 객체에서만 사용 가능
+ - 이 외에도 클래스 선언문에 쓸 수 있는 TYPE 등이 있음
+
+### @interface
+ - 이 파일을 어노테이션 클래스로 지정
+ - LoginUser 라는 이름을 가진 어노테이션이 생성되었다고 보면 됨
 
--- src/main/java/com/goldcrestwilma/springboot/web/IndexController.java
+++ src/main/java/com/goldcrestwilma/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.goldcrestwilma.springboot.web;
 
+import com.goldcrestwilma.springboot.config.auth.LoginUser;
 import com.goldcrestwilma.springboot.config.auth.dto.SessionUser;
 import com.goldcrestwilma.springboot.service.posts.PostsService;
 import com.goldcrestwilma.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
