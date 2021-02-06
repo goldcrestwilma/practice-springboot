058d1da8fca447836c4543741ff5ff479bd765d2
goldcrestwilma <aga601@naver.com>
2021-02-06 17:36:39 +0900

    Add file about process id

--- src/main/java/com/goldcrestwilma/springboot/Application.java
+++ src/main/java/com/goldcrestwilma/springboot/Application.java
@@ -2,12 +2,15 @@ package com.goldcrestwilma.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 }
