139d5cc150635ffda00d9b869700025a1566d492
goldcrestwilma <aga601@naver.com>
2021-02-06 17:55:00 +0900

    Remove pid file

--- src/main/java/com/goldcrestwilma/springboot/Application.java
+++ src/main/java/com/goldcrestwilma/springboot/Application.java
@@ -2,15 +2,11 @@ package com.goldcrestwilma.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.addListeners(new ApplicationPidFileWriter());
-        springApplication.run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
