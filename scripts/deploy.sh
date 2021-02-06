cd49b9be02dbccba25398b04e7c390dd0fe6d0ff
goldcrestwilma <aga601@naver.com>
2021-02-06 16:56:27 +0900

    Update deploy progress for deploy automation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step1
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot-with-AWS
-
-cd $REPOSITORY/$PROJECT_NAME/
-
-echo "> Git Pull"
-
-git pull
-
-echo "> Start Build"
-
-./gradlew build
-
-echo "> Move step1 directory"
-
-cd $REPOSITORY
 
 echo "> Copy Build File"
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> Check Application Pid"
 
@@ -41,10 +27,13 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
+echo "> Add permission"
+
 chmod +x $JAR_NAME
+
+echo "> Run Application"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
